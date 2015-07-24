0b870be511c746b120af63487a68fe696912e63d
839 <8398a7@gmail.com>
2015-07-25 01:30:19 +0900

    implement omniauth

--- lib/omniauth/strategies/linkus.rb
+++ lib/omniauth/strategies/linkus.rb
@@ -3,6 +3,24 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Linkus < OmniAuth::Strategies::OAuth2
+      RAW_INFO_URL = 'api/v1/users/me'
+
+      uid { raw_info['uid'] }
+
+      info do
+        {
+          sid: raw_info['sid'],
+          name: raw_info['name']
+        }
+      end
+
+      extra do
+        { raw_info: raw_info }
+      end
+
+      def raw_info
+        @raw_info ||= JSON.parse(access_token.get(RAW_INFO_URL).response.body)
+      end
     end
   end
 end
